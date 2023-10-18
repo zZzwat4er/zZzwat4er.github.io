f416032152eb966f3bc5b58dc2583a9288a35b9e
zZzwat4er <valenten2001@mail.ru>
2023-10-19 01:03:56 +0300

    aaa

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import axios from 'axios';
+import { useThemeParams } from '@vkruglikov/react-telegram-web-app';
 import './App.css';
 
 import { TodoItem } from './components/TodoItem';
 import Todo from './models/Todo';
 import { useAuthContext } from './hooks/useAuthContext';
 
+export const todoContext = createContext();
+
 function App() {
 
   let [state, setState] = useState([]);
+  let { user, _ } = useAuthContext();
+  let [sheme, themeParams] = useThemeParams();
 
   useEffect(() => {
-    axios.get('https://odd-tan-ox-wig.cyclic.app/tasks')
-      .then(res => {
-        return res.data;
-      })
-      .then(json => {
-        setState(json.map(e => Todo.from(e)));
-      })
-      .catch(e => setState([]));
+    if (user?.id) {
+      axios.get(`https://odd-tan-ox-wig.cyclic.app/tasks/${user.id}`)
+        .then(res => {
+          return res.data;
+        })
+        .then(json => {
+          setState(json.map(e => Todo.from(e)));
+        })
+        .catch(e => setState([]));
+    }
   }, []);
 
-  let {user, _} = useAuthContext();
+
 
   return (
-    <div className="App">
-      <div>
-        <h1>{user?.id ?? 'null'}</h1>
-        {state.map((todo) => <TodoItem todo={todo} />)}
-      </div>
+    <div className="App" style={
+      {
+        backgroundColor: themeParams.bg_color,
+        textDecorationColor: themeParams.text_color
+      }
+    }>
+      <todoContext.Provider value={setState}>
+        {user ? <div>
+          {state.map((todo) => <TodoItem todo={todo} />)}
+        </div>
+          :
+          <h1>Could not get user ID</h1>
+        }
+      </todoContext.Provider>
     </div>
   );
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'
 import Todo from '../models/Todo'
+import { useThemeParams } from '@vkruglikov/react-telegram-web-app'
+import axios, { HttpStatusCode } from 'axios';
+import { todoContext } from '../App';
 
 export function TodoItem({ todo }) {
-    return (
-      <div className='Todo-Item'>
-        <ul >
-            <li className='Main-Text'>{todo.message}</li>
-            <li className='Status'>{todo.status}</li>
-        </ul>
-      </div>
-    )
+  let [sheme, params] = useThemeParams();
+  let setTodo = useContext(todoContext)
+
+  function deleteTodo() {
+    axios.delete(`https://odd-tan-ox-wig.cyclic.app/tasks/${todo.taskId}`)
+      .then(res => {
+        if (res.status !== HttpStatusCode.InternalServerError) {
+          setTodo(prev => prev.filter(e => e.taskId !== todo.taskId));
+        }
+      })
+      .catch(e => { });
+  }
+
+  return (
+    <div className='Todo-Item' style={{
+      background: params.button_color
+    }}>
+      <ul >
+        <li className='Main-Text'>{todo.message}</li>
+        <li onClick={deleteTodo}>{"\u274C"}</li>
+        {/* <li className='Status'>{todo.status}</li> */}
+      </ul>
+    </div>
+  )
 }
